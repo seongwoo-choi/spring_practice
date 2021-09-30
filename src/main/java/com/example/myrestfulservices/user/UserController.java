feb49dffbc13b2d89d650c30dd6284b9f26a0fc9
seongwoo-choi <how0326@naver.com>
2021-09-30 13:56:29 +0900

    HTTP Status Code 제어

--- src/main/java/com/example/myrestfulservices/user/UserController.java
+++ src/main/java/com/example/myrestfulservices/user/UserController.java
@@ -1,7 +1,10 @@
 package com.example.myrestfulservices.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -24,9 +27,18 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User saveUser = service.save(user);
-        return saveUser;
+
+        // server 에서 반환하려는 값을 response 에 담기 위해 ServletUriComponentsBuilder 을 사용한다.
+        // 입력받은 도메인 값 + 만들어진 id 가 포함된 주소를 만들어주는 로직
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(saveUser.getId())
+                .toUri();
+
+        // response 에 location 속성을 만들어서 값을 반환한다.
+        return ResponseEntity.created(location).build();
     }
 
 }
