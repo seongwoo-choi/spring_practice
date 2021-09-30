38e37341a7ceeffdde144592915d5aa981e74bf7
seongwoo-choi <how0326@naver.com>
2021-09-30 13:46:34 +0900

    사용자 등록 API 구현

--- src/main/java/com/example/myrestfulservices/user/UserController.java
+++ src/main/java/com/example/myrestfulservices/user/UserController.java
@@ -1,9 +1,6 @@
 package com.example.myrestfulservices.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +21,12 @@ public class UserController {
     @GetMapping("/users/{id}")
     public User retrieveUsers(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        User saveUser = service.save(user);
+        return saveUser;
     }
 
 }
